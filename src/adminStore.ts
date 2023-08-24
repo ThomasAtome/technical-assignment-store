e0bf60847a6ecc2578d995fe8530cdff99c8869d
Yann Donazzolo <yann@guiild.fr>
2023-08-24 15:50:31 +0200

    first commit

---
+++ src/adminStore.ts
@@ -0,0 +1,22 @@
+import { Restrict, Store } from "./store";
+import { UserStore } from "./userStore";
+
+const credentialStore = new Store();
+credentialStore.writeEntries({ username: "user1" });
+
+export class AdminStore extends Store {
+  @Restrict("r")
+  public user: UserStore;
+  @Restrict()
+  name: string = "John Doe";
+  @Restrict("rw")
+  getCredentials = (): Store => {
+    return credentialStore;
+  };
+
+  constructor(user: UserStore) {
+    super();
+    this.defaultPolicy = "none";
+    this.user = user;
+  }
+}
