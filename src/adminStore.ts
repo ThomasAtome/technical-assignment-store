542926efb3d460aa506deee918ab5fb5e943c1d8
Thomas CHEVALIER <contact@thomaschevalier.fr>
2023-08-30 22:36:58 +0200

    push before submit

--- src/adminStore.ts
+++ src/adminStore.ts
@@ -1,22 +1,24 @@
-import { Restrict, Store } from "./store";
-import { UserStore } from "./userStore";
+import { Restrict, Store } from './store'
+import { UserStore } from './userStore'
 
-const credentialStore = new Store();
-credentialStore.writeEntries({ username: "user1" });
+const credentialStore = new Store()
+credentialStore.writeEntries({ username: 'user1' })
 
 export class AdminStore extends Store {
-  @Restrict("r")
-  public user: UserStore;
-  @Restrict()
-  name: string = "John Doe";
-  @Restrict("rw")
-  getCredentials = (): Store => {
-    return credentialStore;
-  };
+    @Restrict('r')
+    public user: UserStore
 
-  constructor(user: UserStore) {
-    super();
-    this.defaultPolicy = "none";
-    this.user = user;
-  }
+    @Restrict()
+    name: string = 'John Doe'
+
+    @Restrict('rw')
+    getCredentials = (): Store => {
+        return credentialStore
+    }
+
+    constructor (user: UserStore) {
+        super()
+        this.defaultPolicy = 'none'
+        this.user = user
+    }
 }
--- src/store.ts
+++ src/store.ts
@@ -1,53 +1,153 @@
-import { JSONArray, JSONObject, JSONPrimitive } from "./json-types";
+import 'reflect-metadata'
 
-export type Permission = "r" | "w" | "rw" | "none";
+import { JSONArray, JSONObject, JSONPrimitive } from './json-types'
+
+export type Permission = 'r' | 'w' | 'rw' | 'none';
 
 export type StoreResult = Store | JSONPrimitive | undefined;
 
 export type StoreValue =
-  | JSONObject
-  | JSONArray
-  | StoreResult
-  | (() => StoreResult);
+    | JSONObject
+    | JSONArray
+    | StoreResult
+    | (() => StoreResult);
 
 export interface IStore {
-  defaultPolicy: Permission;
-  allowedToRead(key: string): boolean;
-  allowedToWrite(key: string): boolean;
-  read(path: string): StoreResult;
-  write(path: string, value: StoreValue): StoreValue;
-  writeEntries(entries: JSONObject): void;
-  entries(): JSONObject;
+    defaultPolicy: Permission;
+
+    [index:string]: any
+
+    allowedToRead (key: string): boolean;
+
+    allowedToWrite (key: string): boolean;
+
+    read (path: string): StoreResult;
+
+    write (path: string, value: StoreValue): StoreValue;
+
+    writeEntries (entries: JSONObject): void;
+
+    entries (): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
-  throw new Error("Method not implemented.");
+export function Restrict (permission?: Permission): PropertyDecorator {
+    // I prefer to do an extra check as the decorator is responsible for safety.
+    if (permission && !['r', 'w', 'rw', 'none'].includes(permission)) {
+        throw new Error(`Invalid permission: ${permission}`)
+    }
+
+    if (!permission) {
+        permission = 'none'
+    }
+
+    return (target: Object, propertyKey: string | symbol): void => {
+        Reflect.defineMetadata('permission', permission, target, propertyKey)
+    }
 }
 
 export class Store implements IStore {
-  defaultPolicy: Permission = "rw";
+    [key: string]: any
 
-  allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
-  }
+    defaultPolicy: Permission = 'rw'
 
-  allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
-  }
+    allowedToRead (key: string): boolean {
+        const permission = Reflect.getMetadata('permission', this, key)
+        if(permission) {
+            return permission.includes('r')
+        }
+        return this.defaultPolicy.includes('r')
+    }
 
-  read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
-  }
+    allowedToWrite (key: string): boolean {
+        const permission = Reflect.getMetadata('permission', this, key)
+        if(permission) {
+            return permission.includes('w')
+        }
+        return this.defaultPolicy.includes('w')
+    }
 
-  write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
-  }
+    read(path: string): StoreResult {
+        const keys = path.split(':');
+        const key = keys[0];
 
-  writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
-  }
+        // If we have a complexe path and the primary key is instanceof Store, all the nested permission are in the nested Store instance
+        if (keys.length > 1 && this[key] instanceof Store) {
+            return this[key].read(keys.slice(1).join(':'));
+        }
 
-  entries(): JSONObject {
-    throw new Error("Method not implemented.");
-  }
+        if (!this.allowedToRead(keys[0])) {
+            throw new Error('No permission for read');
+        }
+
+        const result = keys.length > 1 ? this.performRead(keys, this) : this[key]
+
+        return result instanceof Function ? result() : result;
+    }
+
+    write(path: string, value: StoreValue): StoreValue {
+        const keys = path.split(':');
+        const key = keys[0];
+
+        // If we have a complexe path and the primary key is instanceof Store, all the nested permission are in the nested Store instance
+        if (keys.length > 1 && this[key] instanceof Store) {
+            return this[key].write(keys.slice(1).join(':'), value);
+        }
+
+        if (!this.allowedToWrite(keys[0])) {
+            throw new Error('No permission for write');
+        }
+
+        this.performWrite(keys, value, this);
+
+        return value;
+    }
+
+    writeEntries (entries: JSONObject): void {
+        // Probably we can enhance it, but not sure about one of the test i commented
+        for (const entry in entries) {
+            this[entry] = entries[entry]
+        }
+    }
+
+    entries (): JSONObject {
+        const keys = Object.keys(this)
+        const entries: JSONObject = {}
+        for (const key of keys) {
+            // Only check if already have metadata for this key and after check the permission
+            if (Reflect.getMetadata('permission', this, key) && this.allowedToRead(key)) {
+                entries[key] = this[key]
+            }
+        }
+        return entries
+    }
+
+    private performRead(keys: string[], current: any): StoreResult | Function {
+        const key = keys[0];
+
+        if (current[key] instanceof Store) {
+            return this.performRead(keys.slice(1), current[key]);
+        } else if (current[key] instanceof Function) {
+            return keys.length === 1 ? current[key]() : this.performRead(keys.slice(1), current[key]());
+        } else if (keys.length > 1) {
+            return this.performRead(keys.slice(1), current[key]);
+        }
+
+        return current[key];
+    }
+
+    private performWrite(keys: string[], value: StoreValue, current: any): void {
+        const key = keys[0];
+
+        if (keys.length > 1) {
+            if (current[key] instanceof Store) {
+                this.performWrite(keys.slice(1), value, current[key]);
+            } else {
+                current[key] = current[key] || {};
+                this.performWrite(keys.slice(1), value, current[key]);
+            }
+        } else {
+            current[key] = value;
+        }
+    }
+
 }
